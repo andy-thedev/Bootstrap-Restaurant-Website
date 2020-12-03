38bb4c6e859e879bb56973dcfadd5f2150b95343
Won Tae Jung <wontae.andyjung@gmail.com>
2020-12-02 21:07:36 -0500

    added navigation bar that changes opacity depending on scroll position of user. Also added navigation routing and minor fixes to design

--- js/functionality.js
+++ js/functionality.js
@@ -1,16 +1,28 @@
-$(document).ready(function(){
-    $('.menu_category').click(function(){
-        var category = $(this).attr('id');
-        if (category == 'all') {
-            $('.menu-item').addClass('hide');
-            setTimeout(function(){
-                $('.menu-item').removeClass('hide');
-            }, 300);
-        } else {
-            $('.menu-item').addClass('hide');
-            setTimeout(function(){
-                $('.' + category).removeClass('hide');
-            }, 300);
-        }
-    });
+$(document).ready(
+    function(){
+        $('.menu_category').click(function(){
+            var category = $(this).attr('id');
+            $('.tagsort-active').removeClass('tagsort-active');
+            $(this).addClass('tagsort-active');
+            if (category == 'all') {
+                $('.menu-item').addClass('hide');
+                setTimeout(function(){
+                    $('.menu-item').removeClass('hide');
+                }, 300);
+            } else {
+                $('.menu-item').addClass('hide');
+                setTimeout(function(){
+                    $('.' + category).removeClass('hide');
+                }, 300);
+            }
+        });
+
+        $(window).scroll(function(){
+            var scroll = $(window).scrollTop();
+            if(scroll > 500){
+                $('.navbar').addClass('scrolled');
+            } else{
+                $('.navbar').removeClass('scrolled');
+            }
+        });
 });
